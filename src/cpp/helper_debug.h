936c7c0e50754fe9a97dfc2d8670b6d7b8c86998
Enrico Lattuada <lattuada.enrico@gmail.com>
2022-10-11 13:49:57 +0200

    Add array display to debug helper

--- src/cpp/helper_debug.h
+++ src/cpp/helper_debug.h
@@ -25,4 +25,23 @@ void display_vector(vector<double> *vec,
     }
 }
 
+void display_array(double *vec,
+                   size_t nx,
+                   size_t ny,
+                   size_t nz)
+{
+    for (size_t t = 0; t < nz; t++)
+    {
+        for (size_t y = 0; y < ny; y++)
+        {
+            for (size_t x = 0; x < nx; x++)
+            {
+                cout << vec[t * (nx * ny) + y * nx + x] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
+
 #endif
