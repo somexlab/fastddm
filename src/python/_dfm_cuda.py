4ecb7de529b931aa888dc6f7316b700b39e6a6f5
Enrico Lattuada <lattuada.enrico@gmail.com>
2022-12-05 17:24:31 +0100

    DEV: Add mem check for fft2 in dfm direct

--- src/python/_dfm_cuda.py
+++ src/python/_dfm_cuda.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from ._memchk import get_free_mem
 from ._gpumemchk import get_free_gpu_mem
+from .core_cuda import get_device_pitch, get_device_fft2_mem, get_device_fft_mem
 
 def dfm_direct_gpu(img_seq: np.ndarray, lags: List[int], nx: int, ny: int) -> np.ndarray:
     """Digital Fourier Microscopy, direct mode on GPU
@@ -48,8 +49,9 @@ def dfm_direct_gpu(img_seq: np.ndarray, lags: List[int], nx: int, ny: int) -> np
     mem_gpu = get_free_gpu_mem()[0]   # !! FOR NOW, I FIX THE GPU ID TO 0 !!
     # get memory size of pixel value
     pixel_memsize = img_seq[0,0,0].itemsize
-    # get number of pixels over x and y in one image
+    # get number of pixels over x and y in one image and gpu x-pitch for buffer
     num_pixel_x = img_seq.shape[-1]
+    pitch_x = get_device_pitch(num_pixel_x, img_seq[0,0,0])
     num_pixel_y = img_seq.shape[-2]
     # compute the number of iterations for fft2
     # give priority to number of host/device data transfer
@@ -59,14 +61,16 @@ def dfm_direct_gpu(img_seq: np.ndarray, lags: List[int], nx: int, ny: int) -> np
         num_fft2 += 1
         # compute number of batched fft2
         fft2_batch_len = (len(img_seq) - 1) // num_fft2 + 1
-        # buffer -- Nx * Ny * fft2_batch_len * pixel_memsize
-        mem_gpu_req += num_pixel_x * num_pixel_y * fft2_batch_len * pixel_memsize
+        # buffer -- pitch_x * Ny * fft2_batch_len * pixel_memsize
+        mem_gpu_req += pitch_x * num_pixel_y * fft2_batch_len * pixel_memsize
         # workspace -- (nx // 2 + 1) * ny * fft2_batch_len * 2 * 8bytes
         mem_gpu_req += ((nx // 2) + 1) * ny * fft2_batch_len * 8 * 2
+        # cufft2 internal buffer -- determined by `get_device_fft2_mem`
+        mem_gpu_req += get_device_fft2_mem(nx, ny, fft2_batch_len)
         if mem_gpu > mem_gpu_req:
             break
         if num_fft2 == len(img_seq):
-            raise MemoryError('Not enough space on GPU.')
+            raise MemoryError('Not enough space on GPU for fft2.')
 
     # +++ ANALYZE +++
 
