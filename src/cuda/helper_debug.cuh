c60c1e197812569fec3aefcb51ebd6304c72b7ea
Enrico Lattuada <lattuada.enrico@gmail.com>
2022-12-27 11:28:09 +0100

    FIX: Fix build warnings

--- src/cuda/helper_debug.cuh
+++ src/cuda/helper_debug.cuh
@@ -24,7 +24,7 @@
     {                                         \
         gpuAssert((ans), __FILE__, __LINE__); \
     }
-inline void gpuAssert(cudaError_t code, char *file, int line, bool abort = true)
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true)
 {
     if (code != cudaSuccess)
     {
