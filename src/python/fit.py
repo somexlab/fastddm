43f8e2e402775f7e4b4817fa88b84c82c7ffa5b1
Fabian Krautgasser <fabian.krautgasser@zamg.ac.at>
2023-01-02 23:15:02 +0100

    DEV: add basic parameter estimation for simple structure function

--- src/python/fit.py
+++ src/python/fit.py
@@ -40,11 +40,38 @@ simple_structure_function.set_param_hint("tau", min=0.0, max=np.inf, value=1.0)
 simple_structure_function.set_param_hint("amplitude", min=0.0, max=np.inf, value=1.0)
 
 
+def _simple_structure_function_parameter_helper(
+    xdata: np.ndarray,
+    ydata: np.ndarray,
+) -> lm.Parameters:
+
+    # first estimate B from the constant offset of a 2nd-degree polynomial fit of the first values
+    n = 5
+    poly = np.polynomial.Polynomial.fit(xdata[:n], ydata[:n], deg=2)
+    B = max(poly.coef[0], 0.0)  # first coefficient is constant offset; ensure B >= 0.0
+
+    # estimate A
+    A = max(ydata[-1] - B, 0.0)  # ensure A >= 0.0
+
+    # estimate tau
+    if A > 0.0:
+        tau = xdata[np.argmin(np.abs((ydata - B) / A - 0.5))]
+    else:
+        tau = 1.0
+
+    params = lm.Parameters()
+    for name, val in {"A": A, "B": B, "tau": tau}.items():
+        params.add(name, min=0.0, max=np.inf, value=val)
+
+    return params
+
+
 def fit(
     model: lm.Model,
     xdata: np.ndarray,
     ydata: np.ndarray,
     params: Optional[Union[lm.Parameters, lm.Parameter]] = None,
+    estimate_simple_parameters: bool = False,
     verbose: bool = False,
     **fitargs: Any,
 ) -> lm.model.ModelResult:
@@ -63,6 +90,8 @@ def fit(
         The data we want to fit the model to.
     params : Optional[Union[lm.Parameters, lm.Parameter]], optional
         Either a single lm.Parameter or lm.Parameters, as the Model expects, by default None
+    estimate_simple_parameters : bool
+        Set to true if some simple estimates to get initial values for A, B and tau should be used. These estimates are only really applicable for the simple structure function setting, by default False
     verbose : bool, optional
         Pretty prints the parameters before fitting and the fit report afterwards, by default False
 
@@ -71,14 +100,20 @@ def fit(
     lm.model.ModelResult
         The results of the fit.
     """
+    params = model.make_params() if params is None else params
     if verbose:
         print(":: Model parameters:")
-        p = model.make_params() if params is None else params
-        p.pretty_print()
+        params.pretty_print()
 
     # we will assume the models only have one indep. variable
     indep_var = model.independent_vars[0]
     fitargs[indep_var] = xdata  # mapping xdata to independent variable name
+
+    if estimate_simple_parameters:
+        simple_pars = _simple_structure_function_parameter_helper(xdata, ydata)
+        if verbose:
+            print(":: updating given parameters with simple estimates .. ")
+        params.update(simple_pars)
 
     # fit
     result = model.fit(ydata, params=params, **fitargs)
