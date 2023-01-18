ee590e38689a5d733576ce30088311471f2e6ee6
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-01-18 13:49:41 +0100

    FIX: Fix bug with image sequence reader

--- src/python/_utils.py
+++ src/python/_utils.py
@@ -55,16 +55,16 @@ def images2numpy(fnames : Sequence[str]) -> np.ndarray:
     """
     # open first image
     tmp = io.imread(fnames[0])
-    if len(tmp.shape > 2):
+    if len(tmp.shape) > 2:
         # initialize image sequence array
-        shape = (tmp.shape[2], len(fnames), tmp.shape[:2])
+        shape = (tmp.shape[2], len(fnames), *tmp.shape[:2])
         img_seq = np.zeros(shape=shape, dtype=tmp.dtype)
 
         # read images in c,t,y,x order
         for i, f in enumerate(fnames):
             tmp = io.imread(f)
             for j in range(tmp.shape[2]):
-                img_seq[j,i] = tmp[:,:,j]
+                img_seq[j, i] = tmp[:, :, j]
     else:
         # initialize image sequence array
         shape = (len(fnames), *tmp.shape)
@@ -73,3 +73,5 @@ def images2numpy(fnames : Sequence[str]) -> np.ndarray:
         # read images
         for i, f in enumerate(fnames):
             img_seq[i] = io.imread(f)
+
+    return img_seq
