d74a71d7d36c5c7d389e9a4ab1144f989bc8184d
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-01-25 10:41:28 +0100

    FIX: Fix fallback version

--- setup.py
+++ setup.py
@@ -154,7 +154,7 @@ class CMakeBuild(build_ext):
 setup(
     name = "fastddm",
     use_scm_version = {
-        "fallback_version": "0.1.2",
+        "fallback_version": "0.1.3",
     },
     packages = find_packages(),
     url = 'https://github.com/somexlab/fastddm',
