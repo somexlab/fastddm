351e82a6325196ccaa0693ba1c2ba8f97f80f083
Fabian Krautgasser <fabian.krautgasser@zamg.ac.at>
2023-02-26 21:31:36 +0100

    MNT: added ignore comments for flake8

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,11 +1,9 @@
 """Testing all imports and check if they work."""
 
-import pytest
-
 
 def test_init_import():
-    from fastddm import tiff2numpy, images2numpy, read_images
-    from fastddm import (
+    from fastddm import tiff2numpy, images2numpy, read_images  # noqa: F401
+    from fastddm import (  # noqa: F401
         ImageStructureFunction,
         AzimuthalAverage,
         ddm,
@@ -16,28 +14,37 @@ def test_init_import():
 
 
 def test_weights_import():
-    from fastddm.weights import sector_average_weight, sphere_form_factor
+    from fastddm.weights import sector_average_weight, sphere_form_factor  # noqa: F401
 
 
 def test_window_import():
-    from fastddm.window import blackman, blackman_harris
+    from fastddm.window import blackman, blackman_harris  # noqa: F401
 
 
 def test_mask_import():
-    from fastddm.mask import central_cross_mask
+    from fastddm.mask import central_cross_mask  # noqa: F401
 
 
 def test_lags_import():
-    from fastddm.lags import logspace_int, fibonacci
+    from fastddm.lags import logspace_int, fibonacci  # noqa: F401
 
 
 def test_fit_import():
-    from fastddm.fit import simple_exp_model, simple_structure_function, fit
+    from fastddm.fit import (  # noqa: F401
+        simple_exp_model,
+        simple_structure_function,
+        fit,
+    )
 
 
 def test_utils_import():
-    from fastddm._utils import read_metadata, read_images, images2numpy, tiff2numpy
+    from fastddm._utils import (  # noqa: F401
+        read_metadata,
+        read_images,
+        images2numpy,
+        tiff2numpy,
+    )
 
 
 def test_io_import():
-    from fastddm._io import _store_data, load, _save_as_tiff
+    from fastddm._io import _store_data, load, _save_as_tiff  # noqa: F401
