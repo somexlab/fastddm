7104e15ebe80c670b2743bfec14f221e2ea09709
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-03-14 14:34:01 +0100

    MNT: Group similar models

--- src/python/generate_fit_models.py
+++ src/python/generate_fit_models.py
@@ -11,134 +11,9 @@ if not os.path.exists(model_dir):
     os.mkdir(model_dir)
 
 
-# ** simple exponential model
-def simple_exponential(
-    x: Union[np.ndarray, float],
-    A: float,
-    B: float,
-    Gamma: float
-) -> Union[np.ndarray, float]:
-    """A simple exponential function model for the structure function
-
-    .. math:
-        
-        D(t) = A \\left( 1 - \\exp(- \\Gamma t) \\right) + B
-
-    Parameters
-    ----------
-    x : np.ndarray, float
-        Independent variable.
-    A : float
-        Amplitude.
-    B : float
-        Noise.
-    Gamma : float
-        Relaxation rate.
-
-    Returns
-    -------
-    np.ndarray, float
-        Simple exponential structure function model.
-    """
-    return A*(1-np.exp(-x*Gamma))+B
-
-simple_exponential_model = Model(simple_exponential)
-simple_exponential_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
-simple_exponential_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
-simple_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
-
-save_model(simple_exponential_model, os.path.join(model_dir, 'simple_exponential_model.sav'))
-
-
-# ** stretched exponential model
-def stretched_exponential(
-    x: Union[np.ndarray, float],
-    A: float,
-    B: float,
-    Gamma: float,
-    beta: float
-) -> Union[np.ndarray, float]:
-    """A stretched exponential function model for the structure function
-
-    .. math:
-        
-        D(t) = A \\left( 1 - \\exp(- (\\Gamma t)^{\\beta}) \\right) + B
-
-    where :math:`0 \\le \\beta \\le 1`.
-
-    Parameters
-    ----------
-    x : np.ndarray, float
-        Independent variable.
-    A : float
-        Amplitude.
-    B : float
-        Noise.
-    Gamma : float
-        Relaxation rate.
-    beta : float
-        Stretching exponent.
-
-    Returns
-    -------
-    np.ndarray, float
-        Stretched exponential structure function model.
-    """
-    return A*(1-np.exp(-(x*Gamma)**beta))+B
-
-stretched_exponential_model = Model(stretched_exponential)
-stretched_exponential_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
-stretched_exponential_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
-stretched_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
-stretched_exponential_model.set_param_hint('beta', value=1.0, min=0.0, max=1.0)
-
-save_model(stretched_exponential_model, os.path.join(model_dir, 'stretched_exponential_model.sav'))
-
-
-# ** compressed exponential model
-def compressed_exponential(
-    x: Union[np.ndarray, float],
-    A: float,
-    B: float,
-    Gamma: float,
-    beta: float
-) -> Union[np.ndarray, float]:
-    """A compressed exponential function model for the structure function
-
-    .. math:
-        
-        D(t) = A \\left( 1 - \\exp(- (\\Gamma t)^{\\beta}) \\right) + B
-
-    where :math:`1 \\le \\beta \\le \\infty`.
-
-    Parameters
-    ----------
-    x : np.ndarray, float
-        Independent variable.
-    A : float
-        Amplitude.
-    B : float
-        Noise.
-    Gamma : float
-        Relaxation rate.
-    beta : float
-        Compressing exponent.
-
-    Returns
-    -------
-    np.ndarray, float
-        Compressed exponential structure function model.
-    """
-    return A*(1-np.exp(-(x*Gamma)**beta))+B
-
-compressed_exponential_model = Model(compressed_exponential)
-compressed_exponential_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
-compressed_exponential_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
-compressed_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
-compressed_exponential_model.set_param_hint('beta', value=1.0, min=1.0, max=np.inf)
-
-save_model(compressed_exponential_model, os.path.join(model_dir, 'compressed_exponential_model.sav'))
-
+# -------------------------------------
+# -- IMAGE STRUCTURE FUNCTION MODELS --
+# -------------------------------------
 
 # ** generic exponential model
 def generic_exponential(
@@ -154,7 +29,11 @@ def generic_exponential(
         
         D(t) = A \\left( 1 - \\exp(- (\\Gamma t)^{\\beta}) \\right) + B
 
-    where :math:`0 \\le \\beta \\le \\infty`.
+    where :math:`0 < \\beta < \\infty`.
+
+    Can also be used to define a `simple_exponential_model`, where :math:`\\beta = 1`,
+    a `stretched_exponential_model`, where :math:`0 < \\beta \\le 1`, or
+    a `compressed_exponential_model`, where :math:`1 \\le \\beta < \\infty`.
 
     Parameters
     ----------
@@ -183,6 +62,36 @@ generic_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf
 generic_exponential_model.set_param_hint('beta', value=1.0, min=0.0, max=np.inf)
 
 save_model(generic_exponential_model, os.path.join(model_dir, 'generic_exponential_model.sav'))
+
+
+# ** simple exponential model
+simple_exponential_model = Model(generic_exponential)
+simple_exponential_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
+simple_exponential_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
+simple_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
+simple_exponential_model.set_param_hint('beta', value=1.0, vary=False)
+
+save_model(simple_exponential_model, os.path.join(model_dir, 'simple_exponential_model.sav'))
+
+
+# ** stretched exponential model
+stretched_exponential_model = Model(generic_exponential)
+stretched_exponential_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
+stretched_exponential_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
+stretched_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
+stretched_exponential_model.set_param_hint('beta', value=1.0, min=0.0, max=1.0)
+
+save_model(stretched_exponential_model, os.path.join(model_dir, 'stretched_exponential_model.sav'))
+
+
+# ** compressed exponential model
+compressed_exponential_model = Model(generic_exponential)
+compressed_exponential_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
+compressed_exponential_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
+compressed_exponential_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
+compressed_exponential_model.set_param_hint('beta', value=1.0, min=1.0, max=np.inf)
+
+save_model(compressed_exponential_model, os.path.join(model_dir, 'compressed_exponential_model.sav'))
 
 
 # ** double exponential model
@@ -265,6 +174,13 @@ def flory_schulz(
     with mean decay rate :math:`\\bar{\\Gamma}` and normalized standard deviation :math:`\\sigma=1/\\sqrt{z+1}`
     (see `Mailer et al (2015) <https://iopscience.iop.org/article/10.1088/0953-8984/27/14/145102>`_).
 
+    Can also be used to define an `exponential_distribution_model`, where the
+    decay rates follow an exponential distribution (for :math:`\\sigma=1`, i.e., :math:`z=0`)
+
+    .. math:
+
+        G(\\Gamma) = \\frac{1}{\\bar{\\Gamma}} \\exp\\left(-\\frac{\\Gamma}{\\bar{\\Gamma}}\\right)
+
     Parameters
     ----------
     x : np.ndarray, float
@@ -295,48 +211,10 @@ save_model(flory_schulz_model, os.path.join(model_dir, 'flory_schulz_model.sav')
 
 
 # ** exponential distribution model
-def exponential_distribution(
-    x: Union[np.ndarray, float],
-    A: float,
-    B: float,
-    Gamma: float
-) -> Union[np.ndarray, float]:
-    """An exponential distribution function model for the structure function
-
-    .. math:
-        
-        D(t) = A \\left( 1 - (1 + \\bar{\\Gamma} t)^{-1} \\right) + B
-
-    The decay rates follow an exponential distribution
-
-    .. math:
-
-        G(\\Gamma) = \\frac{1}{\\bar{\\Gamma}} \\exp\\left(-\\frac{\\Gamma}{\\bar{\\Gamma}}\\right)
-
-    with mean decay rate :math:`\\bar{\\Gamma}`
-    (see `Mailer et al (2015) <https://iopscience.iop.org/article/10.1088/0953-8984/27/14/145102>`_).
-
-    Parameters
-    ----------
-    x : np.ndarray, float
-        Independent variable.
-    A : float
-        Amplitude.
-    B : float
-        Noise.
-    Gamma : float
-        Relaxation rate.
-
-    Returns
-    -------
-    np.ndarray, float
-        Exponential distribution structure function model.
-    """
-    return A*(1 - 1/(1 + Gamma * x)) + B
-
-exponential_distribution_model = Model(exponential_distribution)
+exponential_distribution_model = Model(flory_schulz)
 exponential_distribution_model.set_param_hint('A', value=1.0, min=0.0, max=np.inf)
 exponential_distribution_model.set_param_hint('B', value=0.0, min=-np.inf, max=np.inf)
 exponential_distribution_model.set_param_hint('Gamma', value=1.0, min=0.0, max=np.inf)
+exponential_distribution_model.set_param_hint('sigma', value=1.0, vary=False)
 
 save_model(exponential_distribution_model, os.path.join(model_dir, 'exponential_distribution_model.sav'))
