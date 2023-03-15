04f66c0b3d0782b0371eb029768659ed311216db
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-03-15 23:35:23 +0100

    DEV: Add data writer

--- src/python/_ddmsfwriter.py
+++ src/python/_ddmsfwriter.py
@@ -13,6 +13,7 @@ from sys import byteorder
 from .imagestructurefunction import ImageStructureFunction
 
 VERSION = (0, 1)
+HEADER_BYTE_LEN = 64
 
 format_byte_len = {
     'B' : 1,    # unsigned char
@@ -31,7 +32,6 @@ class DdmSFWriter:
     version = VERSION
 
     def __init__(self, file : str):
-        self.offset = 0
         # create directory if it does not exist
         if not path.exists(path.dirname(file)):
             makedirs(path.dirname(file))
@@ -64,19 +64,30 @@ class DdmSFWriter:
         if self._fh is not None:
             self._fh.close()
 
-    def write_file(self, image_structure_function : ImageStructureFunction) -> None:
+    def write_file(
+        self,
+        image_structure_function : ImageStructureFunction
+        ) -> None:
+        """Write image structure function to binary file
+
+        Parameters
+        ----------
+        image_structure_function : ImageStructureFunction
+            Image structure function object.
+        """
         Nt, Ny, Nx = image_structure_function.shape
         Nextra = 2  # power spectrum + var
         dtype = 0   # WE DON'T HAVE FOR THE MOMENT A SINGLE PRECISION OPTION, SO WE FIX DOUBLE FOR NOW
         self.write_header(Nt, Ny, Nx, Nextra, dtype)
+        self.write_data(image_structure_function)
 
     def write_header(
-            self,
-            Nt : int,
-            Ny : int,
-            Nx : int,
-            Nextra : int,
-            dtype : int
+        self,
+        Nt : int,
+        Ny : int,
+        Nx : int,
+        Nextra : int,
+        dtype : int
         ) -> None:
         """Write image structure function file header
 
@@ -95,46 +106,108 @@ class DdmSFWriter:
             0 : float64
             1 : float32
         """
-        self.offset = 0
+        offset = 0
         # write system endianness
         # 2 char, bytes 0-1
         if byteorder == 'little':
             self._fh.write('II'.encode("utf-8"))
         else:
             self._fh.write('MM'.encode("utf-8"))
-        self.offset += 2 * format_byte_len['B']
+        offset += 2 * format_byte_len['B']
 
         # write file identifier
         # unsigned short, bytes 2-3
         self._fh.write(struct.pack('H', 73))
-        self.offset += format_byte_len['H']
+        offset += format_byte_len['H']
 
         # write file version
         # 2 unsigned char, bytes 4-5
         self._fh.write(struct.pack('BB', *(self.version)))
-        self.offset += 2 * format_byte_len['B']
+        offset += 2 * format_byte_len['B']
 
         # write length of the image structure function data array
         # unsigned int, bytes 6-9
         self._fh.write(struct.pack('I', Nt))
-        self.offset += format_byte_len['I']
+        offset += format_byte_len['I']
 
         # write height of the image structure function data array
         # unsigned int, bytes 10-13
         self._fh.write(struct.pack('I', Ny))
-        self.offset += format_byte_len['I']
+        offset += format_byte_len['I']
 
         # write width of the image structure function data array
         # unsigned int, bytes 14-17
         self._fh.write(struct.pack('I', Nx))
-        self.offset += format_byte_len['I']
+        offset += format_byte_len['I']
 
         # write number of extra data slices packed (i.e., variance and power spectrum)
         # unsigned int, bytes 18-21
         self._fh.write(struct.pack('I', Nextra))
-        self.offset += format_byte_len['I']
+        offset += format_byte_len['I']
 
         # write dtype identifier
         # unsigned char, byte 22
         self._fh.write(struct.pack('B', dtype))
-        self.offset += format_byte_len['B']
+        offset += format_byte_len['B']
+
+        # add empty bytes up to 63 for future use (if needed)
+        self._fh.write(bytearray(HEADER_BYTE_LEN - offset))
+
+    def write_data(
+        self,
+        image_structure_function : ImageStructureFunction
+        ) -> None:
+        """Write the data to binary file.
+
+        Parameters
+        ----------
+        image_structure_function : ImageStructureFunction
+            Image structure function object.
+        """
+        # FOR THE MOMENT WE ONLY HAVE DOUBLE (FLOAT64) VALUES
+        Nt, Ny, Nx = image_structure_function.shape
+
+        # write data
+        for val in image_structure_function._data.flat:
+            self._fh.write(struct.pack('d', val))
+        data_offset = HEADER_BYTE_LEN
+        extra_offset = data_offset + format_byte_len['d'] * Nt * Ny * Nx
+        
+        # write kx
+        for kx in image_structure_function.kx:
+            self._fh.write(struct.pack('d', kx))
+        kx_offset = extra_offset + format_byte_len['d'] * 2 * Ny * Nx
+
+        # write ky
+        for ky in image_structure_function.ky:
+            self._fh.write(struct.pack('d', ky))
+        ky_offset = kx_offset + format_byte_len['d'] * Nx
+
+        # write tau
+        for tau in image_structure_function.tau:
+            self._fh.write(struct.pack('d', tau))
+        tau_offset = ky_offset + format_byte_len['d'] * Ny
+
+        # write pixel size
+        self._fh.write(struct.pack('d', image_structure_function.pixel_size))
+        pixel_size_offset = tau_offset + format_byte_len['d'] * Nt
+
+        # write delta t
+        self._fh.write(struct.pack('d', image_structure_function.delta_t))
+        delta_t_offset = pixel_size_offset + format_byte_len['d']
+
+        # write offsets
+        # data offset
+        self._fh.write(struct.pack('Q', data_offset))
+        # extra offset
+        self._fh.write(struct.pack('Q', extra_offset))
+        # kx offset
+        self._fh.write(struct.pack('Q', kx_offset))
+        # ky offset
+        self._fh.write(struct.pack('Q', ky_offset))
+        # tau offset
+        self._fh.write(struct.pack('Q', tau_offset))
+        # pixel size offset
+        self._fh.write(struct.pack('Q', pixel_size_offset))
+        # delta t offset
+        self._fh.write(struct.pack('Q', delta_t_offset))
