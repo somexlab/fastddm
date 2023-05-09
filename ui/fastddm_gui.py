ad21d08e05c4b5aabe68ac96abad6a0466756707
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-05-09 12:03:41 +0200

    DEV: Allow to load ui without conversion

--- ui/fastddm_gui.py
+++ ui/fastddm_gui.py
@@ -1,19 +1,26 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from main import Ui_MainWindow
+from PyQt6 import uic
+from PyQt6 import QtWidgets
 
 
-class FastDDMApp(Ui_MainWindow):
-    def __init__(self, window):
-        self.setupUi(window)
+class FastDDMApp(QMainWindow):
+    def __init__(self):
+        super(FastDDMApp, self).__init__()
+
+        # load the ui file
+        uic.loadUi("main.ui", self)
+
+        # show the ui
+        self.show()
 
 
+# initialize the app
 app = QApplication(sys.argv)
 MainWindow = QMainWindow()
 
 # create instance of app
-ui = FastDDMApp(MainWindow)
+ui = FastDDMApp()
 
 # show the window and start the app
-MainWindow.show()
 app.exec()
