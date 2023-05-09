8def857d38347916b3ebab790966842449a46cc1
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-05-09 16:48:20 +0200

    DEV: Add dummy tab loader

--- ui/fastddm_gui.py
+++ ui/fastddm_gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt6 import uic
 from PyQt6 import QtWidgets
 
@@ -11,8 +11,44 @@ class FastDDMApp(QMainWindow):
         # load the ui file
         uic.loadUi("main.ui", self)
 
+        self.show_message("FastDDM application started...")
+
+        # show to status bar when tabs are changed
+        self.tabWidget.currentChanged.connect(self.tab_changed)
+
+        # add tabs
+        self.load_tab_widget()
+        self.load_tab_widget()
+
         # show the ui
         self.show()
+
+    def show_message(self, text):
+        self.statusBar().showMessage(text)
+
+    def tab_changed(self):
+        # get current tab index
+        idx = self.tabWidget.currentIndex()
+        self.show_message(f"Switched to tab {idx}")
+
+    def load_tab_widget(self):
+        # create dummy tab an title
+        tab = QtWidgets.QWidget()
+        title = "Dummy tab"
+
+        # add grid layout to tab
+        grid_layout = QtWidgets.QGridLayout(tab)
+        # add vertical layout in grid layout
+        vertical_layout = QtWidgets.QVBoxLayout()
+        grid_layout.addLayout(vertical_layout, 0, 0, 1, 1)
+
+        # add widget to tab
+        widget = QtWidgets.QPushButton(tab)
+        widget.setText(f"Push Button {self.tabWidget.count()}")
+        vertical_layout.addWidget(widget)
+
+        # add tab
+        self.tabWidget.addTab(tab, title)
 
 
 # initialize the app
