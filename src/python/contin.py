e4ff11df5747bcf002db9a7f0e9de8b8f33c1235
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-05-21 17:31:59 +0200

    DEV: Add _run_contin to run contin executable

--- src/python/contin.py
+++ src/python/contin.py
@@ -2,3 +2,33 @@
 # Part of FastDDM, released under the GNU GPL-3.0 License.
 # Authors: Enrico Lattuada
 # Maintainers: Enrico Lattuada
+
+import os
+import numpy as np
+
+from fastddm.imagestructurefunction import ImageStructureFunction
+from fastddm.azimuthalaverage import AzimuthalAverage
+
+this_file_path = os.path.abspath(os.path.dirname(__file__))
+contin_exec_path = os.path.join(this_file_path, "contin")
+
+def _run_contin(
+        input_file: str,
+        output_file: str
+        ) -> None:
+    """Run CONTIN executable.
+
+    Executes the command:
+    contin < input_file > output_file
+
+    Parameters
+    ----------
+    input_file : str
+        Input file to contin executable
+    output_file : str
+        File to which contin output is redirected
+    """
+    exec_file = contin_exec_path
+    full_comm = exec_file + " < " + input_file + " > " + output_file
+
+    os.system(full_comm)
