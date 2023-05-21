4c4a2828d8e8e3240268e67970cd1aef69cf8995
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-05-21 20:52:22 +0200

    DEV: Add empty _write_header and notes

--- src/python/contin.py
+++ src/python/contin.py
@@ -13,6 +13,60 @@ from fastddm.azimuthalaverage import AzimuthalAverage
 this_file_path = os.path.abspath(os.path.dirname(__file__))
 contin_exec_path = os.path.join(this_file_path, "contin")
 
+"""Parameters summary (see contin-manual1.pdf)
+LAST:       if TRUE, CONTIN stops after analysis (1 = TRUE, 0 = FALSE)
+GMNMX, 1:   first grid point in the quadrature
+GMNMX, 2:   last point in the quadrature
+IWT:        1 = unweighted analysis
+            2 = error proportional to sqrt(y(tk)), i.e., Poisson statistics
+            3 = error proportional to |y(tk)|, i.e., constant relative error
+            4 = input wk in Card set 7
+            5 = computing wk in USERWT
+NERFIT:     number of residuals used to compute ERRFIT
+NINTT:      number of intervals for tk
+NLINF:      NL in Eq. 3.1-2
+IFORMY:     Fortran FORMAT specification enclosed in parentheses for yk
+            in Card Set 6
+IFORMT:     Fortran FORMAT specification enclosed in parentheses for tk
+            in Card Set 5b
+IFORMW:     Fortran FORMAT specification enclosed in parentheses for wk
+            in Card Set 7
+NG:         Number of quadrature grid points, if Eq.3.1-2 is solved by
+            quadrature. Nx if Eq.3.1-1 is solved directly
+DOUSNQ:     if TRUE, USERNQ is to be called to specify inequality constraints
+            (1 = TRUE, 0 = FALSE)
+USERNQ:     sets the inequality constraints in Eq.3.1-4
+NONNEG:     if TRUE, constrains s(gm) in Eq.3.1-3 to be non-negative
+            (1 = TRUE, 0 = FALSE)
+NORDER:     choice of the regularizors
+            < 0: for calling USERRG to set a special user-defined regularizor
+            0-5: for setting the regularizor (sum in Eq.3.2.2-1) to be the sums
+            of the squares of the nth differences of the Ng-n sets (similar to
+            order of derivative). 0 is sum xj^2, 2 is sum of second derivative
+            squared (smooth solution)
+RUSER(10):  0: yk are not changed
+            < 0: yk are replaced by yk^1/2
+            > 0: yk are replaced by (yk/R10-1)^1/2
+RUSER(15):  medium refractive index.
+RUSER(16):  wavelength illumination source (in nm).
+            If R16=0, R20 is not computed and R21 is set to 0.
+RUSER(17):  scattering angle (in degrees).
+RUSER(18):  absolute temperature.
+RUSER(19):  viscosity (in cP).
+RUSER(20):  scattering vector (in cm^-1). Computed from R15, R16, R17.
+IUSER(10):  1: s(lambda) is weight fraction molecular weight distribution.
+               R23=1, R22=R18*R20^2, so that D=R18*lambda^R22
+            2: s(lambda) is diffusion coefficient distribution.
+               R23=0, R22=1, R21=R20^2
+            3: s(lambda) is weight fraction radius distribution (in cm)
+               of spheres satisfying the Stokes-Einstein relation.
+               R23=3, R22=-1, R21=kB*R18*R20^2/(0.06*pi*R19)
+            4: generic case where R21, R22, and R23 are set by the user
+
+Kernels are of the form
+F(lambda_m, tk) = f_m^2 lambda_m^R23 exp(-R21 tk lambda_m^R22)
+"""
+
 
 def _generate_input_file(
         file: str,
@@ -34,6 +88,12 @@ def _generate_input_file(
         # write header
         # write data
         _write_data(fh, xdata, ydata)
+
+
+def _write_header(
+        fh: TextIO
+        ) -> None:
+    pass
 
 
 def _write_data(
