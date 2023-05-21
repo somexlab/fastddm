22f5300e39812b01a8d73f8c9dcca978390e67a3
Enrico Lattuada <lattuada.enrico@gmail.com>
2023-05-21 18:33:00 +0200

    DEV: Add _generate_input_file and _write_data

--- src/python/contin.py
+++ src/python/contin.py
@@ -3,6 +3,7 @@
 # Authors: Enrico Lattuada
 # Maintainers: Enrico Lattuada
 
+from typing import Union, Optional, List, TextIO
 import os
 import numpy as np
 
@@ -11,6 +12,51 @@ from fastddm.azimuthalaverage import AzimuthalAverage
 
 this_file_path = os.path.abspath(os.path.dirname(__file__))
 contin_exec_path = os.path.join(this_file_path, "contin")
+
+
+def _generate_input_file(
+        file: str,
+        xdata: Union[np.ndarray, List],
+        ydata: Union[np.ndarray, List]
+        ) -> None:
+    """Generate contin input file with parameters and data.
+
+    Parameters
+    ----------
+    file : str
+        File name
+    xdata : Union[np.ndarray, List]
+        x data
+    ydata : Union[np.ndarray, List]
+        y data
+    """
+    with open(file, 'w') as fh:
+        # write header
+        # write data
+        _write_data(fh, xdata, ydata)
+
+
+def _write_data(
+        fh: TextIO,
+        xdata: Union[np.ndarray, List],
+        ydata: Union[np.ndarray, List]
+        ) -> None:
+    """Write contin input data to file.
+
+    Parameters
+    ----------
+    file : TextIO
+        File handle
+    xdata : Union[np.ndarray, List]
+        x data
+    ydata : Union[np.ndarray, List]
+        y data
+    """
+    full_data = np.concatenate((xdata, ydata))
+
+    for d in full_data:
+        fh.write(f"{d:>15.8E}\n")
+
 
 def _run_contin(
         input_file: str,
@@ -32,3 +78,5 @@ def _run_contin(
     full_comm = exec_file + " < " + input_file + " > " + output_file
 
     os.system(full_comm)
+
+
