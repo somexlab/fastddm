e46676210a80686e0fc2b7dda1cc5bd8079875c0
moyuanchen <62314859+moyuanchen@users.noreply.github.com>
2023-06-05 12:35:04 +0200

    COM: changed file name to avoid confusion

---
+++ src/python/bidisperse_big_fitting_residue.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023-2023 University of Vienna, Enrico Lattuada, Fabian Krautgasser, and Roberto Cerbino.
+# Part of FastDDM, released under the GNU GPL-3.0 License.
+# Authors: Mike Chen 
+
+from .azimuthalaverage import AzimuthalAverage
+from fastddm import fit
+
+def calculate_residue(model, xdata, ydata):
+
+
+
+    return xdata, residue
